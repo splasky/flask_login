41eee8a139d2aeec6cd51dab7fd158ad161daaf4
splasky <henrychung860326@gmail.com>
2018-05-20 01:27:47 +0800

    Update size of password column

--- src/create_db.sql
+++ src/create_db.sql
@@ -3,6 +3,6 @@ CREATE DATABASE AwsFinalProject;
 CREATE TABLE Accounts(
     user_id BIGINT NULL AUTO_INCREMENT,
     user_name VARCHAR(20) NULL,
-    user_password VARCHAR(20) NULL,
+    user_password VARCHAR(100) NULL,
     PRIMARY KEY (user_id)
 );
